83611d6ce46bacbeb6c3370788c887645225c2a7
Prajithmaddy <31074187+Prajithmaddy@users.noreply.github.com>
2017-08-27 00:09:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css.'));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 
-app.get('/article-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+
 });
 // Use 8080 only for local development if you already have apache running on 80
 
