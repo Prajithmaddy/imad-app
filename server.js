fb13602e79b19b4ac469e21187f623ea2259bda9
Prajithmaddy <31074187+Prajithmaddy@users.noreply.github.com>
2017-08-27 17:13:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/main.js', function (req, res) {
@@ -18,7 +18,7 @@ app.get('/ui/main.js', function (req, res) {
 }); 
   
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
